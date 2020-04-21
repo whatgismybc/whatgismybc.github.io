5d9584871666ed6e2aa2a189a1b842f00d528ac9
Divya Shanmugam <divyas@mit.edu>
2020-04-20 20:58:18 -0400

    flask frozen files; not so hoefpul

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class bc_form(FlaskForm):
-    pill_name = StringField('Pill name', validators=[DataRequired()])
-    submit = SubmitField('find out!')
